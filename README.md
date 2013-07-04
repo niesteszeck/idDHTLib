64d4292c095cbb3bbf20837dc90390f379437253
Nicolás Sanz <nsanz.cl@gmail.com>
2013-07-04 00:03:27 -0400

    first commit. NOT TESTED

---
+++ README.md
@@ -0,0 +1,24 @@
+idDHTLib
+=======
+
+Interrupt driven DHT11 & DHT22 library
+
+	VERSION: 	0.0.1
+
+	PURPOSE: 	Interrupt driven Lib for DHT11 and DHT22 with Arduino.
+
+	LICENCE:	GPL v3 (http://www.gnu.org/licenses/gpl.html)
+
+	DATASHEET: http://www.micro4you.com/files/sensor/DHT11.pdf
+
+	DATASHEET: http://www.adafruit.com/datasheets/DHT22.pdf
+	
+	Based on idDHT11 library: https://github.com/niesteszeck/idDHT11
+
+	Based on DHTLib library: http://playground.arduino.cc/Main/DHTLib
+
+	Based on code proposed: http://forum.arduino.cc/index.php?PHPSESSID=j6n105kl2h07nbj72ac4vbh4s5&topic=175356.0
+
+
+This lib is a beta state 'cause I don't have a DHT22 sensor, so anyone that can try 
+it with a DHT22 sensor it will be great
---
+++ idDHTLib.h
@@ -0,0 +1,80 @@
+/*
+	FILE: 		idDHTlib.h
+	VERSION: 	0.0.1
+	PURPOSE: 	Interrupt driven Lib for DHT11 and DHT22 with Arduino.
+	LICENCE:	GPL v3 (http://www.gnu.org/licenses/gpl.html)
+	DATASHEET: http://www.micro4you.com/files/sensor/DHT11.pdf
+	DATASHEET: http://www.adafruit.com/datasheets/DHT22.pdf
+	
+	Based on idDHT11 library: https://github.com/niesteszeck/idDHT11
+	Based on DHTLib library: http://playground.arduino.cc/Main/DHTLib
+	Based on code proposed: http://forum.arduino.cc/index.php?PHPSESSID=j6n105kl2h07nbj72ac4vbh4s5&topic=175356.0
+
+*/
+
+
+#ifndef idDHTLib_H__
+#define idDHTLib_H__
+
+#if defined(ARDUINO) && (ARDUINO >= 100)
+#include <Arduino.h>
+#else
+#include <WProgram.h>
+#endif
+
+#define IDDHTLIB_VERSION "0.0.1"
+
+// state codes
+#define IDDHTLIB_OK			0
+#define IDDHTLIB_ACQUIRING		1
+#define IDDHTLIB_ACQUIRED		2
+#define IDDHTLIB_RESPONSE_OK		3
+
+// error codes
+#define IDDHTLIB_ERROR_CHECKSUM		-1
+#define IDDHTLIB_ERROR_TIMEOUT		-2
+#define IDDHTLIB_ERROR_ACQUIRING	-3
+#define IDDHTLIB_ERROR_DELTA		-4
+#define IDDHTLIB_ERROR_NOTSTARTED	-5
+
+#define IDDHTLIB_CHECK_STATE		if(state == STOPPED)			\
+						return status;			\
+					else if(state != ACQUIRED)		\
+						return IDDHTLIB_ERROR_ACQUIRING;
+									
+class idDHTLib
+{
+public:
+	idDHTLib(int pin, int intNumber,void (*isrCallback_wrapper)());
+    	void init(int pin, int intNumber,void (*isrCallback_wrapper)());
+	void dht11Callback();
+	void dht22Callback();
+	int acquire();
+	int acquireAndWait();
+	float getCelsius();
+	float getFahrenheit();
+	float getKelvin();
+	double getDewPoint();
+	double getDewPointSlow();
+	float getHumidity();
+	bool acquiring();
+	int getStatus();
+	
+private:
+	
+	void (*isrCallback_wrapper)(void);
+	
+	enum states{RESPONSE=0,DATA=1,ACQUIRED=2,STOPPED=3,ACQUIRING=4};
+	volatile states state;
+	volatile int status;
+	volatile byte bits[5];
+	volatile byte cnt;
+	volatile byte idx;
+	volatile int us;
+	int intNumber;
+	int pin;
+	volatile float hum;
+	volatile float temp;
+	void isrCallback(bool dht22);
+};
+#endif // idDHTLib_H__
